54fd8c8c0e8b1b6be2f7fd93a623231c1da57f6a
Piotr Gęga <piotrgega@gmail.com>
2012-02-03 22:26:49 +0100

    rails sample app should be shared, modified files structure of test dir, let's try to use shared test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,2 @@
 require 'bundler/setup'
 require 'minitest/autorun'
-
-
--- test/version_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper'))
-
-class VersionTest < MiniTest::Unit::TestCase
-
-  def test_that_kitty_can_eat
-    refute_nil Vertebrae::VERSION
-  end
-
-end
