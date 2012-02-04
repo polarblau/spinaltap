fad4fb4a60caf1002711988010c64a2f6f4bef74
Polarblau <polarblau@gmail.com>
2012-02-04 15:09:50 +0200

    Attempting to use guard-minitest.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
 require 'bundler/setup'
 require 'minitest/autorun'
 
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
 
+require File.expand_path("../rails_app/config/environment.rb",  __FILE__)
+require "rails/test_help"
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+# For Generators
+require 'rails/generators/test_case'
