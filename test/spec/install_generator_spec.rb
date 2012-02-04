15f411a2f7115ed8dc00f8c621dc5ed1fabea9c5
Polarblau <polarblau@gmail.com>
2012-02-04 20:14:04 +0200

    Started to work on install generator spec.

---
+++ test/spec/install_generator_spec.rb
@@ -0,0 +1,17 @@
+require 'minitest/spec'
+ 
+describe Vertebrae::Generators::InstallGenerator do
+  
+  before do
+    @generator = Vertebrae::Generators::InstallGenerator.new
+  end
+
+  describe "folder structure creation" do
+    
+    it "should create a models folder" do
+    
+    end
+
+  end
+
+end
