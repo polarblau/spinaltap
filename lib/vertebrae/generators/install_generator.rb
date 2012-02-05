25158f2a1233c2423810453173abf28480ca00ca
Polarblau <polarblau@gmail.com>
2012-02-05 14:26:12 +0200

    Fixed indentation. [skip ci].

--- lib/vertebrae/generators/install_generator.rb
+++ lib/vertebrae/generators/install_generator.rb
@@ -1,11 +1,11 @@
 module Vertebrae
-	module Generators
-		class InstallGenerator < Rails::Generators::Base
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
 
       def create_dir_layout
         empty_directory "app/assets/javascripts/models"
       end
-			
-		end
-	end
+
+    end
+  end
 end
