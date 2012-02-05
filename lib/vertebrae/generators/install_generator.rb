51bde4c2b7f837aadd179e56c6c9a1db2b18917f
Polarblau <polarblau@gmail.com>
2012-02-05 14:39:42 +0200

    Tested general structure generation.

--- lib/vertebrae/generators/install_generator.rb
+++ lib/vertebrae/generators/install_generator.rb
@@ -5,6 +5,8 @@ module Vertebrae
       def create_dir_layout
         empty_directory "app/assets/javascripts/models"
         empty_directory "app/assets/javascripts/collections"
+        empty_directory "app/assets/javascripts/views"
+        empty_directory "app/assets/javascripts/templates"
       end
 
     end
--- test/unit/vertebrae/generators/install_generator_test.rb
+++ test/unit/vertebrae/generators/install_generator_test.rb
@@ -5,14 +5,24 @@ class InstallGeneratorTest < Rails::Generators::TestCase
   destination File.expand_path("../tmp", File.dirname(__FILE__))
   #teardown :cleanup_destination_root
 
-  def test_should_create_a_models_folder
+	def test_should_create_a_models_folder
     run_generator
     assert_directory "app/assets/javascripts/models"
   end
 
-	def test_should_create_a_collections_folder
+  def test_should_create_a_collections_folder
     run_generator
     assert_directory "app/assets/javascripts/collections"
+  end
+
+  def test_should_create_a_views_folder
+    run_generator
+    assert_directory "app/assets/javascripts/views"
+  end
+
+    def test_should_create_a_templates_folder
+    run_generator
+    assert_directory "app/assets/javascripts/templates"
   end
 
 end
