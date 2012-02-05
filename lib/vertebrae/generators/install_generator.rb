46b74d33d02b72bad2b168cbd5fd3f3d1e1e5047
Polarblau <polarblau@gmail.com>
2012-02-05 17:02:48 +0200

    Refactoring.

--- lib/vertebrae/generators/install_generator.rb
+++ lib/vertebrae/generators/install_generator.rb
@@ -1,31 +1,31 @@
-require 'active_support'
+require File.expand_path(File.join(File.dirname(__FILE__), 'resource_helpers'))
 
 module Vertebrae
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include ActiveSupport
+      include Vertebrae::Generators::ResourceHelpers
 
       source_root File.expand_path("../templates", __FILE__)
 
       def create_app_initializer_file
-        template "app.coffee", "app/assets/javascripts/app.js.coffee"
+        template "app.coffee", "#{javascripts_path}/app.js.coffee"
       end
 
       def create_app_router_file
-        template "routes.coffee", "app/assets/javascripts/config/app.js.coffee"
+        template "routes.coffee", "#{javascripts_path}/config/app.js.coffee"
       end
 
       def create_app_base_files
         %w(models collections views).each do |directory|
           file = directory[0..-2]
-          path = "app/assets/javascripts/#{directory}/base_#{file}.js.coffee"
+          path = "#{javascripts_path}/#{directory}/base_#{file}.js.coffee"
           template "base_#{file}.coffee", path
         end  
       end
 
       def create_app_folder_structure
         %w(models collections views templates config).each do |directory|
-          empty_directory "app/assets/javascripts/#{directory}"
+          empty_directory "#{javascripts_path}/#{directory}"
         end
       end
 
---
+++ lib/vertebrae/generators/resource_helpers.rb
@@ -0,0 +1,11 @@
+module Vertebrae
+  module Generators
+    module ResourceHelpers
+      
+      def javascripts_path
+        "app/assets/javascripts"
+      end
+      
+    end
+  end
+end
