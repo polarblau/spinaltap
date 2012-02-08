30ba9fb29f2ab47824c07048aea12e63fee643f0
Polarblau <polarblau@gmail.com>
2012-02-08 18:19:08 +0200

    Fixed typo causing the sky to fall down.

--- test/unit/vertebrae/generators/model_generator_test.rb
+++ test/unit/vertebrae/generators/model_generator_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'generators_test_helper'))
 
-class InstallGeneratorTest < Rails::Generators::TestCase
+class ModelGeneratorTest < Rails::Generators::TestCase
   include GeneratorsTestHelper
   tests Vertebrae::Generators::ModelGenerator
   
@@ -17,19 +17,18 @@ class InstallGeneratorTest < Rails::Generators::TestCase
     assert_file "#{javascripts_path}/models/bar/baz/bat/foo.js.coffee"
   end
 
-  # should contain model name
+  # # should contain model name
 
   def test_should_create_a_model_file_with_correct_name
     Rails.application.class.stubs(:name).returns("TestApp::Application")
     run_generator %w(foo)
-    assert_file "#{javascripts_path}/models/foo.js.coffee", /class\ TestApp\.Models.Foo/ 
+    assert_file "#{javascripts_path}/models/foo.js.coffee", /class TestApp.Models.Foo/ 
   end
  
   def test_should_create_a_model_file_with_correctly_nested_name
     Rails.application.class.stubs(:name).returns("TestApp::Application")
     run_generator %w(bar/baz/bat/foo)
-    assert_file "#{javascripts_path}/models/foo.js.coffee", /class\ TestApp\.Models.Bar.Baz.Bat.Foo/ 
+    assert_file "#{javascripts_path}/models/bar/baz/bat/foo.js.coffee", /class TestApp.Models.Bar.Baz.Bat.Foo/ 
   end
  
-
 end
