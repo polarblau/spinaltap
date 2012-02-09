cdf6d7eb1a69420aa1b290455f66d18cefda573b
Polarblau <polarblau@gmail.com>
2012-02-09 18:38:28 +0200

    Trying to override generators, VI.

--- lib/vertebrae/railtie.rb
+++ lib/vertebrae/railtie.rb
@@ -3,7 +3,13 @@ require 'rails'
 module Vertebrae
   class Railtie < Rails::Railtie
 
-    config.app_generators.orm :vertebrae
+    #config.app_generators.orm :vertebrae
     
+  end
+end
+
+module Rails::Generators
+  class ModelGenerator < Base
+    hook_for :vertebrae
   end
 end
