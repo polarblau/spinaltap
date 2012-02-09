8a3fc8fedf0100fe0287c99f4da27fea6ae422d8
Polarblau <polarblau@gmail.com>
2012-02-09 14:10:31 +0200

    Moved engine into own file.

--- lib/vertebrae.rb
+++ lib/vertebrae.rb
@@ -1,14 +1,7 @@
 require "rails/generators"
 require "vertebrae/version"
+require 'vertebrae/engine'
 require "vertebrae/generators/generator_helpers"
 require "vertebrae/generators/named_generator_helpers"
 require "vertebrae/generators/install/install_generator"
 require "vertebrae/generators/model/model_generator"
-
-# should this go into it's own file?
-require 'rails'
-# http://guides.rubyonrails.org/asset_pipeline.html#adding-assets-to-your-gems
-module Vertebrae
-  class Engine < Rails::Engine
-  end
-end
