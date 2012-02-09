7b08eed1a9682d8c17c021089491075100053b16
Polarblau <polarblau@gmail.com>
2012-02-09 12:48:53 +0200

    Added Rails engine.

--- lib/vertebrae.rb
+++ lib/vertebrae.rb
@@ -4,3 +4,11 @@ require "vertebrae/generators/generator_helpers"
 require "vertebrae/generators/named_generator_helpers"
 require "vertebrae/generators/install/install_generator"
 require "vertebrae/generators/model/model_generator"
+
+# should this go into it's own file?
+require 'rails'
+# http://guides.rubyonrails.org/asset_pipeline.html#adding-assets-to-your-gems
+module Vertebrae
+  class Engine < Rails::Engine
+  end
+end
