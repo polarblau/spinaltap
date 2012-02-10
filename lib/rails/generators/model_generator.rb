d5b2878901aafe72da630c1a5bdb7682044c6f16
Polarblau <polarblau@gmail.com>
2012-02-10 14:56:06 +0200

    Generating collection AND model with rails.

--- lib/rails/generators/model_generator.rb
+++ lib/rails/generators/model_generator.rb
@@ -5,10 +5,10 @@ module Rails
       class_option  :skip_vertebrae, 
                     :type    => :boolean, 
                     :default => false,
-                    :desc    => "Don't generate a backbone model file?"
+                    :desc    => "Don't generate a backbone collection and model?"
 
       def create_vertebrae_model
-        invoke "vertebrae:model" unless options.skip_vertebrae?
+        invoke "vertebrae:collection" unless options.skip_vertebrae?
       end
 
     end
