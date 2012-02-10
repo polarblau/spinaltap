fac27e83b7a9434cf0ffef914ee09d78f868138a
Polarblau <polarblau@gmail.com>
2012-02-10 11:29:19 +0200

    Moved rails generator extension into own folder.

---
+++ lib/rails/generators/model_generator.rb
@@ -0,0 +1,18 @@
+require 'rails'
+
+module Rails
+  module Generators
+    class ModelGenerator < NamedBase
+
+      class_option  :skip_vertebrae, 
+                    :type    => :boolean, 
+                    :default => false,
+                    :desc    => "Don't generate a backbone model file?"
+
+      def create_vertebrae_model
+        invoke "vertebrae:model" unless options.skip_vertebrae?
+      end
+
+    end
+  end
+end
--- lib/vertebrae/railtie.rb
+++ lib/vertebrae/railtie.rb
@@ -6,20 +6,3 @@ module Vertebrae
     # config.app_generators.orm :vertebrae    
   end
 end
-
-module Rails
-  module Generators
-    class ModelGenerator < NamedBase
-
-      class_option  :skip_vertebrae, 
-                    :type    => :boolean, 
-                    :default => false,
-                    :desc    => "Don't generate a model file as well?"
-
-      def create_vertebrae_model
-        invoke "vertebrae:model" unless options.skip_vertebrae?
-      end
-
-    end
-  end
-end
