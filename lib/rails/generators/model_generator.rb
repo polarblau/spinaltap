5ef2186a0c32cbde728c287b543163d129acfc1d
Polarblau <polarblau@gmail.com>
2012-02-10 22:50:32 +0200

    Made options a bit more clear for rails generators.

--- lib/rails/generators/model_generator.rb
+++ lib/rails/generators/model_generator.rb
@@ -2,13 +2,13 @@ module Rails
   module Generators
     class ModelGenerator < NamedBase
 
-      class_option  :skip_vertebrae, 
+      class_option  :skip_vertebrae_model, 
                     :type    => :boolean, 
                     :default => false,
                     :desc    => "Don't generate a backbone collection and model?"
 
       def create_vertebrae_model
-        invoke "vertebrae:collection" unless options.skip_vertebrae?
+        invoke "vertebrae:collection" unless options.skip_vertebrae_model?
       end
 
     end
--- lib/rails/generators/resource_generator.rb
+++ lib/rails/generators/resource_generator.rb
@@ -2,8 +2,13 @@ module Rails
   module Generators
     class ResourceGenerator < ModelGenerator
 
+      class_option  :skip_vertebrae_view, 
+              :type    => :boolean, 
+              :default => false,
+              :desc    => "Don't generate a backbone view and template?"
+
       def create_vertebrae_view
-        invoke "vertebrae:view" unless options.skip_vertebrae?
+        invoke "vertebrae:view" unless options.skip_vertebrae_view?
       end
 
     end
