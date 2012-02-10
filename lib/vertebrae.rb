115b029aa2f9030c8664096f3a43288f29bfafc1
Polarblau <polarblau@gmail.com>
2012-02-10 14:55:35 +0200

    Removed railtie require.

--- lib/vertebrae.rb
+++ lib/vertebrae.rb
@@ -3,10 +3,8 @@ require "rails/generators/model_generator"
 
 require "vertebrae/version"
 require "vertebrae/engine"
-require "vertebrae/railtie"
 require "vertebrae/generators/generator_helpers"
 require "vertebrae/generators/named_generator_helpers"
 require "vertebrae/generators/install/install_generator"
 require "vertebrae/generators/model/model_generator"
 require "vertebrae/generators/collection/collection_generator"
-
